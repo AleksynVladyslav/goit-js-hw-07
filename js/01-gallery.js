7f097ca42b6c30fa44edc0eeab542e2d798e3c7a
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-05-11 21:57:38 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,7 +9,7 @@ function createListItemsMurkup(items) {
   return items
     .map(
       item =>
-        `<li class = 'gallery__item'><a class="gallery__link"><img class = "gallery__image" src="${item.preview}" data-source = "${item.original}" alt = "${item.description}" ></a></li>`
+        `<li class = 'gallery__item'><a class="gallery__link" href = "${item.original}"><img class = "gallery__image" src="${item.preview}" data-source = "${item.original}" alt = "${item.description}" ></a></li>`
     )
     .join('');
 }
