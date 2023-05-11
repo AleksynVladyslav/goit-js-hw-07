8ed5d5e69df1c29ab22332b499f2fc4d82251782
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-05-11 21:43:01 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,44 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// import * as basicLightbox from 'basiclightbox';
 
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
-//Ссылки на элементы разметки
-const refs = {
-  listRef: document.querySelector('.gallery'),
-};
-
-const listItemsMurkup = createListItemsMurkup(galleryItems);
-
-addListItemsMurkup(listItemsMurkup);
-
-//Добавляем слушателя
-refs.listRef.addEventListener('click', onSelectImage);
-
-//функция возвращающая строки разметки
+//функция возвращающая строку разметки
 function createListItemsMurkup(items) {
   return items
     .map(
-      item => `<li><img src="${item.preview}" alt = "${item.description}"></li>`
+      item =>
+        `<li class = 'gallery__item'><a class="gallery__link"><img class = "gallery__image" src="${item.preview}" data-source = "${item.original}" alt = "${item.description}" ></a></li>`
     )
     .join('');
 }
+//В переменную listItemsMurkup добавляем разметку
+const listItemsMurkup = createListItemsMurkup(galleryItems);
 
-//Функция добавляющая разметку в DOM
-function addListItemsMurkup(murkup) {
-  refs.listRef.innerHTML = murkup;
+//Ссылки на список в разметке
+const galleryRef = document.querySelector('.gallery');
+
+//Функция добавляющая эл-ты в DOM
+function addListItemsMurkup() {
+  galleryRef.innerHTML = listItemsMurkup;
+}
+//Вызываем addListItemsMurkup
+addListItemsMurkup();
+
+//добавляем слушателя
+function addGalleryItemsListener() {
+  galleryRef.addEventListener('click', event => {
+    if (event.target.nodeName !== 'IMG') {
+      return;
+    }
+    onGalleryItemClick(event);
+  });
 }
 
-//Callback функция
-function onSelectImage(event) {
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-  //   const instance = basicLightbox.create(`
-  //     <img src="${galleryItems[0].original}" width="800" height="600">
-  // `);
-
-  //   instance.show();
+//CallBack функция
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  const imageSrc = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+        <img src="${imageSrc}" width="800" height="600">
+      `);
+  instance.show();
 }
+
+//Вызов функции addGalleryItemsListener
+addGalleryItemsListener();
